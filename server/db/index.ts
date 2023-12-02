190fc3630fcb4ff90724b324c4cadbc52363c9e6
itsmeajay1699 <ajayroy03377@gmail.com>
2023-12-02 18:48:17 +0530

    correcting the showing the email

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,18 +1,17 @@
 import mongoose, { mongo } from "mongoose";
 // Define mongoose schemas
 const todoSchema = new mongoose.Schema({
-    title: {type: String},
-    description: String,
-    done: Boolean,
-    userId: String
-  });
+  title: { type: String },
+  description: String,
+  done: Boolean,
+  userId: String,
+});
 
 const userSchema = new mongoose.Schema({
-  username: {type: String},
+  username: { type: String },
   password: String,
 });
-  
-export const Todo = mongoose.models.Todo || mongoose.model('Todo', todoSchema);
-  
-export const User = mongoose.models.Todo || mongoose.model('User', userSchema);
-  
+
+export const Todo = mongoose.model("Todo", todoSchema);
+
+export const User = mongoose.model("User", userSchema);
